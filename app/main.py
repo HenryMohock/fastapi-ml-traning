088ba3f9d2ab81cc55812d1aec9b2a6a0c3f8af7
HenryMohock <61605922+HenryMohock@users.noreply.github.com>
2024-07-27 19:35:51 +0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi.responses import JSONResponse
 from textdistance import (hamming, mlipns, levenshtein, damerau_levenshtein, jaro_winkler, strcmp95, needleman_wunsch,
                           gotoh, smith_waterman,
                           jaccard, sorensen, tversky, overlap, tanimoto, cosine,monge_elkan, bag,
@@ -13,7 +14,7 @@ from app.api.api import api_router
 from app.api.heartbeat import heartbeat_router
 from app.core.config import settings
 from app.core.event_handler import start_app_handler, stop_app_handler
-from fastapi.staticfiles import StaticFiles  # для публикации index.html
+from fastapi.staticfiles import StaticFiles  # для публікації index.html
 
 from app.homeworks.homework_09_classifier.Classifier import NLTKClassifier, CsvPreProcessing
 from nltk.sentiment import SentimentIntensityAnalyzer
@@ -23,7 +24,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 from pydantic import BaseModel
-from typing import List, Dict
 from gensim.models.doc2vec import Doc2Vec
 from sklearn.cluster import KMeans
 import os
@@ -32,7 +32,6 @@ from collections import Counter
 from typing import List, Dict, Tuple
 from nltk.corpus import stopwords
 
-import pandas as pd
 import re
 import nltk
 from nltk.tokenize import word_tokenize
@@ -40,9 +39,11 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 import spacy
 
-# Импорт необходимых библиотек для модели NLLB
+# Імпорт необхідних бібліотек для моделі NLLB
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig
-import torch
+
+import speech_recognition as sr
+from translate import Translator
 
 
 app = FastAPI(title=settings.PROJECT_NAME)
@@ -54,8 +55,8 @@ app.add_event_handler("startup", start_app_handler(app, settings.MODEL_PATH))
 app.add_event_handler("shutdown", stop_app_handler(app))
 
 # =========================================================
-# Монтирование статических файлов (например, HTML, CSS, JS)
-# Добавлено, чтобы опубликовать на localhost страницы index.html
+# Монтування статичних файлів (наприклад, HTML, CSS, JS)
+# Додано, щоб опублікувати на localhost сторінки index.html
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
@@ -239,40 +240,40 @@ def get_data(word_count: int = 0):
     encoding = 'utf8'
     lang = 'english'
 
-    # Путь к файлу CSV
+    # Шлях до файлу CSV
     file_path = "../../data/IMDB Dataset.csv"
 
-    # Имя извлекаемой колонки файла CSV
+    # Ім'я вилученої колонки файлу CSV
     column_name = 'review'
 
-    # Регулярное выражение для удаления прочих символов
+    # Регулярний вираз для видалення інших символів
     regular_expression = r'blah|_|fffc|br|oz'
 
-    # Порог встречаемости слов
+    # Поріг зустрічаємості слів
     threshold = 6
 
-    # Метод обрезки слов (lemmatization или стемминг)
+    # Метод обрізки слів (лемматизація або стеммінг)
     method = 'lemmatization'
 
-    # Инициализация экземпляра CsvPreProcessing
+    # Ініціалізація екземпляру CsvPreProcessing
     csv_processor = CsvPreProcessing(file_path, column_name, regular_expression, threshold, method, encoding, lang)
 
-    # Получаем текст из CsvPreProcessing
+    # Получаем текст з CsvPreProcessing
     text = csv_processor.get_text
     if word_count != 0:
         text = " ".join(text.split()[:word_count])
 
-    # Создание объекта для анализа тональности
+    # Створення об'єкта для аналізу тональності
     sia = SentimentIntensityAnalyzer()
     sentiment_labels = []
 
-    # Разбиваем текст на слова
-    words = text.split()  # word_tokenize(text)
+    # Розбиваємо текст на слова
+    words = text.split()
 
-    # Анализируем настроение каждого слова и добавляем метку в список
+    # Аналізуємо настрій кожного слова і додаємо метку в список
     for word in words:
         sentiment_score = sia.polarity_scores(word)
-        # Определяем метку настроения на основе compound score
+        # Определяем метку настроения на базі compound score
         if sentiment_score['compound'] >= 0.05:
             label = 'positive'
         elif sentiment_score['compound'] <= -0.05:
@@ -304,7 +305,7 @@ def train_classify_models(data):
 async def train_classify_models_endpoint(data: dict):
     results = train_classify_models(data)
     if results is None:
-        raise HTTPException(status_code=400, detail="Invalid method specified")
+        raise HTTPException(status_code=400, detail="Вказано недійсний метод")
 
     return results
 
@@ -313,10 +314,10 @@ async def train_classify_models_endpoint(data: dict):
 # ==========================================================
 # ==========================================================
 
-# Загрузить список стоп-слов
+# Завантажити список стоп-слів
 stop_words = set(stopwords.words('english'))
 
-# Загрузим ранее обученную модель
+# Завантажимо раніше навчену модель
 model_path = 'models/doc2vec/doc2vec_combined.model'
 try:
     if os.path.isfile(model_path):
@@ -346,26 +347,26 @@ def determine_optimal_clusters(vectors: List, texts: List[str], max_clusters: in
             else:
                 silhouette_scores.append(float('-inf'))  # Для несовместимых значений
 
-        # Возвращаем количество кластеров с наибольшим силуэтным индексом
+        # Повертаємо кількість кластерів із найбільшим силуетним індексом
         optimal_clusters = range(2, min(max_clusters + 1, len(vectors)))[silhouette_scores.index(max(silhouette_scores))]
 
-        # Кластеризация для получения имен тем
+        # Кластеризація для отримання імен тем
         kmeans = KMeans(n_clusters=optimal_clusters, random_state=0)
         kmeans.fit(vectors)
         labels = kmeans.labels_
 
-        # Получение наиболее частых пар слов для каждого кластера
-        # Слова фильтруются по длине (не менее 6 букв) и исключаются стоп-слова.
-        # Находятся две наиболее частые пары слов для каждого кластера.
-        # Если найдено менее двух подходящих слов, добавляется только имеющиеся слова или пустая строка,
-        # если нет подходящих слов.
+        # Отримання найчастіших пар слів кожного кластера
+        # Слова фільтруються за довжиною (не менше 6 літер) та виключаються стоп-слова.
+        # Знаходяться дві найчастіші пари слів кожного кластера.
+        # Якщо знайдено менше двох відповідних слів, додається лише наявні слова або порожній рядок,
+        # якщо немає відповідних слів.
         topic_words = []
         for i in range(optimal_clusters):
             cluster_texts = [texts[j] for j in range(len(texts)) if labels[j] == i]
             all_words = ' '.join(cluster_texts).split()
-            # Фильтруем слова, исключая стоп-слова и слова короче 6 букв
+            # Фільтруємо слова, виключаючи стоп-слова та слова коротше 6 букв
             filtered_words = [word for word in all_words if word.lower() not in stop_words and len(word) >= 6]
-            # Получаем две наиболее частые пары слов
+            # Отримуємо дві найчастіші пари слів
             most_common_words = [word for word, count in Counter(filtered_words).most_common(2)]
             if len(most_common_words) == 2:
                 topic_words.append(' '.join(most_common_words))
@@ -380,21 +381,21 @@ def determine_optimal_clusters(vectors: List, texts: List[str], max_clusters: in
 @app.post("/group_sentences/")
 async def group_sentences(request: SentencesRequest):
     try:
-        # Загрузить ранее сохраненную модель
+        # Завантажити раніше збережену модель
         model = Doc2Vec.load(model_path)
 
         # Получение векторов для предложений
         vectors = [model.infer_vector(sentence.split()) for sentence in request.sentences]
 
-        # Определение оптимального количества кластеров и их имен
+        # Визначення оптимальної кількості кластерів та їх імен
         num_clusters, topic_names = determine_optimal_clusters(vectors, request.sentences, request.max_clusters)
 
-        # Кластеризация предложений
+        # Кластеризація пропозицій
         kmeans = KMeans(n_clusters=num_clusters, random_state=0)
         kmeans.fit(vectors)
         labels = kmeans.labels_
 
-        # Группировка предложений по темам
+        # Угруповання пропозицій за темами
         grouped_sentences = {}
         for label, sentence in zip(labels, request.sentences):
             topic = topic_names[label]
@@ -411,55 +412,55 @@ async def group_sentences(request: SentencesRequest):
 # ==========================================================
 
 def spacy_preprocessing_text(text):
-    # Загрузка модели SpaCy
+    # Завантаження моделі SpaCy
     nlp = spacy.load("en_core_web_sm")
 
-    # Функция для обработки части текста
+    # Функція обробки частини тексту
     def process_text_chunk(chunk):
         doc = nlp(chunk)
         tokens = [token.text for token in doc if not token.is_stop and token.is_alpha]
         return tokens
 
-    # Функция для лемматизации части текста
+    # Функція для лематизації частини тексту
     def lemmatize_text_chunk(chunk):
         doc = nlp(chunk)
         return [token.lemma_ for token in doc]
 
-    # Обработка текста с помощью регулярного выражения
+    # Обробка тексту за допомогою регулярного виразу
     regex_patterns = r'blah|_|fffc|br|oz|< />|<br />|\d+|\W+'
     text = re.sub(regex_patterns, ' ', text)
 
-    # Разбиваем текст на части по 100000 символов
+    # Розбиваємо текст на частини по 100 000 символів
     chunk_size = 100000
     chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
 
-    # Обрабатываем каждую часть и объединяем результаты
+    # Обробляємо кожну частину та об'єднуємо результати
     all_tokens = []
     for chunk in chunks:
         all_tokens.extend(process_text_chunk(chunk))
 
-    # Подсчет частоты слов
+    # Підрахунок частоти слів
     word_freq = Counter(all_tokens)
 
-    # Определение редких слов (слова, которые встречаются менее 5 раз)
+    # Визначення рідкісних слів (слова, що зустрічаються менше ніж 5 разів)
     rare_words = {word for word, count in word_freq.items() if count < 5}
 
-    # Удаление редких слов
+    # Видалення рідкісних слів
     filtered_tokens = [word for word in all_tokens if word not in rare_words]
 
-    # Разбиваем текст на части по 100000 символов для лемматизации
+    # Розбиваємо текст на частини по 100000 символів для лематизації
     filtered_text = ' '.join(filtered_tokens)
     filtered_chunks = [filtered_text[i:i + chunk_size] for i in range(0, len(filtered_text), chunk_size)]
 
-    # Лемматизация каждой части и объединение результатов
+    # Лематизація кожної частини та об'єднання результатів
     lemmatized_tokens = []
     for chunk in filtered_chunks:
         lemmatized_tokens.extend(lemmatize_text_chunk(chunk))
 
-    # Объединение обработанных токенов в строку
+    # Об'єднання оброблених токенів у рядок
     cleaned_text = ' '.join(lemmatized_tokens)
 
-    # Возврат обработанного текста
+    # Повернення обробленого тексту
     return cleaned_text
 
 
@@ -468,40 +469,39 @@ def nltk_preprocessing_text(text):
     nltk.download('stopwords')
     nltk.download('wordnet')
 
-    # Преобразование текста к нижнему регистру
+    # Перетворення тексту на нижній регістр
     text = text.lower()
 
-    # Токенизация текста с использованием регулярного выражения
+    # Токенізація тексту з використанням регулярного виразу
     regex_patterns = r'blah|_|fffc|br|oz|< />|<br />|\d|\w+'
     tokenizer = RegexpTokenizer(regex_patterns)
-    tokens = tokenizer.tokenize(text)
 
-    # Токенизация текста
+    # Токенізація тексту
     tokens = word_tokenize(text)
 
-    # Загрузка стоп-слов
+    # Завантаження стоп-слів
     stop_words = set(stopwords.words('english'))
 
-    # Удаление стоп-слов
+    # Видалення стоп-слів
     tokens = [word for word in tokens if word.isalpha() and word not in stop_words]
 
-    # Подсчет частоты слов
+    # Підрахунок частоти слів
     word_freq = Counter(tokens)
 
-    # Определение редких слов (слова, которые встречаются менее 5 раз)
+    # Визначення рідкісних слів (слова, що зустрічаються менше ніж 5 разів)
     rare_words = {word for word, count in word_freq.items() if count < 5}
 
-    # Удаление редких слов
+    # Видалення рідкісних слів
     tokens = [word for word in tokens if word not in rare_words]
 
-    # Лемматизация
+    # Лематизація
     lemmatizer = WordNetLemmatizer()
     tokens = [lemmatizer.lemmatize(word) for word in tokens]
 
-    # Объединение обработанных токенов в строку
+    # Об'єднання оброблених токенів у рядок
     cleaned_text = ' '.join(tokens)
 
-    # Возврат обработанного текста
+    # Повернення обробленого тексту
     return cleaned_text
 
 
@@ -520,7 +520,7 @@ async def spacy_preprocessing(request: DataRequest):
     response_data = {'spacy_cleaned_text': spacy_preprocessing_text(request.data)}
     return response_data
 
-# POST REQUEST 5 (translator_nllb, переводчик с использованием модели NLLB)
+# POST REQUEST 5 (translator_nllb, перекладач із використанням моделі NLLB)
 # ==========================================================
 # ==========================================================
 
@@ -528,10 +528,10 @@ async def spacy_preprocessing(request: DataRequest):
 class ChatbotRequest(BaseModel):
     input_text: str
     source_language = "eng_Latn"
-    target_language: str = "ukr_Cyrl"  # Язык целевой модели
+    target_language: str = "ukr_Cyrl"  # Мова цільової моделі
 
 
-# Загрузка модели и токенизатора NLLB
+# Завантаження моделі та токенізатора NLLB
 tokenizer_nllb = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M")
 model_nllb = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M")
 
@@ -569,11 +569,104 @@ async def chatbot_endpoint(request: ChatbotRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
 
+
+# POST REQUEST 6 (розпізнавання аудіо англійської мови та переклад її на українську)
+# ==========================================================
+# ==========================================================
+
+
+# Функція для перетворення аудіо на текст
+def audio_to_text(audio_file_path):
+    """
+    Функція audio_to_text(audio_file_path) приймає шлях до аудіофайлу та перетворює аудіо в текст,
+
+    використовуючи сервіс Google для розпізнавання мовлення.
+
+    Параметри:
+        audio_file_path (str): Шлях до аудіофайлу.
+
+    Повертає:
+        str: Розпізнаний текст з аудіофайлу або повідомлення про помилку.
+
+    Функція працює таким чином:
+    1. Створює об'єкт розпізнавача мовлення.
+    2. Відкриває аудіофайл за заданим шляхом.
+    3. Записує аудіодані з файлу.
+    4. Виконує розпізнавання мовлення за допомогою сервісу Google.
+    5. Якщо розпізнавання вдалося, повертає текст.
+    6. Якщо аудіо не вдалося розпізнати, повертає повідомлення "Не вдалося розпізнати аудіо".
+    7. Якщо сталася помилка сервісу розпізнавання мовлення, повертає повідомлення про помилку з детальною інформацією.
+    """
+
+    recognizer = sr.Recognizer()
+
+    with sr.AudioFile(audio_file_path) as source:
+        audio_data = recognizer.record(source)
+
+        try:
+            text = recognizer.recognize_google(audio_data, language="en-EN")
+            return text
+        except sr.UnknownValueError:
+            return "Не вдалося розпізнати аудіо"
+        except sr.RequestError as e:
+            return f"Помилка сервісу розпізнавання мовлення; {e}"
+
+
+# Функція для перекладу тексту
+def translate_text(text, dest_language="uk"):
+    """
+    Функція translate_text(text, dest_language="uk") приймає текст і перекладає його на вказану мову.
+
+    Параметри:
+        text (str): Текст, який необхідно перекласти.
+        dest_language (str, опціонально): Цільова мова перекладу. За замовчуванням "uk" (українська).
+
+    Повертає:
+        str: Перекладений текст або повідомлення про помилку.
+
+    Функція працює таким чином:
+    1. Створює об'єкт перекладача з вказаною цільовою мовою.
+    2. Виконує переклад заданого тексту.
+    3. Якщо переклад успішний, повертає перекладений текст.
+    4. Якщо сталася помилка під час перекладу, повертає повідомлення "Помилка перекладу" з детальною інформацією про помилку.
+    """
+
+    translator = Translator(to_lang=dest_language)
+    try:
+        translation = translator.translate(text)
+        return translation
+    except Exception as e:
+        return f"Помилка перекладу: {e}"
+
+
+@app.post("/speech_to_text/")
+async def speech_to_text(file: UploadFile = File(...)):  # process_audio
+    try:
+        # Зберігаємо завантажений файл
+        with open(f"temp_{file.filename}", "wb") as f:
+            f.write(await file.read())
+
+        # Преобразуем аудіо в текст
+        original_text = audio_to_text(f"temp_{file.filename}")
+
+        # Переводим текст на украинский язык
+        translated_text = translate_text(original_text)
+
+        # Видаляємо тимчасовий файл
+        os.remove(f"temp_{file.filename}")
+
+        return JSONResponse(content={
+            "original_text": original_text,
+            "translated_text": translated_text
+        })
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 # ==========================================================
 # ==========================================================
 
 if __name__ == "__main__":
-    # Use this for debugging purposes only
+    # Використовуйте це лише для налагодження
     import uvicorn
 
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
